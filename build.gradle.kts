a9f0f31fdedd2f9b2ed6d3e4354cfa7d2dcc9d75
MinhDat1312 <nguyenthangdat84@gmail.com>
2025-10-28 14:10:35 +0700

    feat: setup library

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.5.6"
 	id("io.spring.dependency-management") version "1.1.7"
+	id("io.freefair.lombok") version "8.6"
 }
 
 group = "fit.se"
@@ -20,7 +21,35 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6")
+	implementation("com.turkraft.springfilter:jpa:3.2.1")
+	implementation("org.springframework.boot:spring-boot-starter-mail")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.8")
+	runtimeOnly ("com.h2database:h2")
+
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	runtimeOnly("com.mysql:mysql-connector-j")
+	implementation("mysql:mysql-connector-java:8.0.33")
+
+	// https://mvnrepository.com/artifact/com.cloudinary/cloudinary-http44
+	implementation ("com.cloudinary:cloudinary-http44:1.39.0")
+	// https://mvnrepository.com/artifact/commons-io/commons-io
+	implementation("commons-io:commons-io:2.19.0")
+
+	// https://mvnrepository.com/artifact/commons-net/commons-net
+	implementation("commons-net:commons-net:3.12.0")
+	// https://mvnrepository.com/artifact/dnsjava/dnsjava
+	implementation("dnsjava:dnsjava:3.6.3")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.security:spring-security-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
