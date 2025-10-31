ddf0e816a794ee899646619418addf761651468a
trgcghai <conghai.tpma@gmail.com>
2025-10-31 15:17:19 +0700

    feat: remove unused imports from BlogService and CommentService for cleaner code

--- src/main/java/iuh/fit/goat/service/BlogService.java
+++ src/main/java/iuh/fit/goat/service/BlogService.java
@@ -5,8 +5,6 @@ import iuh.fit.goat.dto.response.ResultPaginationResponse;
 import iuh.fit.goat.entity.Blog;
 import iuh.fit.goat.entity.Comment;
 import iuh.fit.goat.entity.Notification;
-import iuh.fit.goat.repository.CommentRepository;
-import iuh.fit.goat.repository.NotificationRepository;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 
--- src/main/java/iuh/fit/goat/service/CommentService.java
+++ src/main/java/iuh/fit/goat/service/CommentService.java
@@ -2,13 +2,8 @@ package iuh.fit.goat.service;
 
 import iuh.fit.goat.dto.response.ResultPaginationResponse;
 import iuh.fit.goat.entity.Comment;
-import iuh.fit.goat.entity.Notification;
-import iuh.fit.goat.repository.CommentRepository;
-import iuh.fit.goat.repository.NotificationRepository;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
-
-import java.util.List;
 
 public interface CommentService {
     Comment handleCreateComment(Comment comment);
