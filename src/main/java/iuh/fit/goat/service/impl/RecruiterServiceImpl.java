5b8815264c8a7f0ccb4768d83d950c69df0995f6
Cheems423 <letuan.le7777@gmail.com>
2025-11-04 15:33:56 +0700

    feat(controller): implement RecruiterController with CRUD and pagination

--- src/main/java/iuh/fit/goat/service/impl/RecruiterServiceImpl.java
+++ src/main/java/iuh/fit/goat/service/impl/RecruiterServiceImpl.java
@@ -13,7 +13,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
-
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
