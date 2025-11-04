3b3ecc5deb4befe39eedc5bbb26085a307029756
MinhDat1312 <nguyenthangdat84@gmail.com>
2025-11-04 12:52:55 +0700

    refactor: format response

---
+++ src/main/java/iuh/fit/goat/util/FormatResponse.java
@@ -0,0 +1,55 @@
+package iuh.fit.goat.util;
+
+import iuh.fit.goat.dto.response.RestResponse;
+import iuh.fit.goat.util.annotation.ApiMessage;
+import jakarta.annotation.Nullable;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.MethodParameter;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+@ControllerAdvice
+public class FormatResponse implements ResponseBodyAdvice<Object> {
+
+    @Override
+    public boolean supports(MethodParameter returnType, Class converterType) {
+        return true;
+    }
+
+    @Override
+    @Nullable
+    public Object beforeBodyWrite(@Nullable Object body, MethodParameter returnType,
+              MediaType selectedContentType, Class selectedConverterType,
+              ServerHttpRequest request, ServerHttpResponse response) {
+
+        HttpServletResponse httpResponse = ((ServletServerHttpResponse) response).getServletResponse();
+        int statusCode = httpResponse.getStatus();
+
+        RestResponse<Object> restResponse = new RestResponse<Object>();
+        restResponse.setStatusCode(statusCode);
+
+        if(body instanceof String || body instanceof Resource){
+            return body;
+        }
+
+        String path = request.getURI().getPath();
+        if(path.startsWith("/v3/api-docs") || path.startsWith("/swagger-ui")){
+            return body;
+        }
+
+        if(statusCode >= 400){
+            return body;
+        } else {
+            restResponse.setData(body);
+            ApiMessage apiMessage = returnType.getMethodAnnotation(ApiMessage.class);
+            restResponse.setMessage( apiMessage != null ? apiMessage.value() : "Success");
+        }
+
+        return restResponse;
+    }
+}
