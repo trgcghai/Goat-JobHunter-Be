9dcbadf09fa2f818b8aefb9274dc127fa7e111ce
MinhDat1312 <nguyenthangdat84@gmail.com>
2025-11-26 17:55:48 +0700

    refactor: improve update application logic and send email

--- src/main/java/iuh/fit/goat/service/ApplicationService.java
+++ src/main/java/iuh/fit/goat/service/ApplicationService.java
@@ -1,6 +1,8 @@
 package iuh.fit.goat.service;
 
+import iuh.fit.goat.dto.request.ApplicationIdsRequest;
 import iuh.fit.goat.dto.response.ApplicationResponse;
+import iuh.fit.goat.dto.response.ApplicationStatusResponse;
 import iuh.fit.goat.dto.response.ResultPaginationResponse;
 import iuh.fit.goat.entity.Applicant;
 import iuh.fit.goat.entity.Application;
@@ -8,10 +10,13 @@ import iuh.fit.goat.entity.Job;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface ApplicationService {
     ApplicationResponse handleCreateApplication(Application application);
 
-    ApplicationResponse handleUpdateApplication(Application application);
+    List<ApplicationStatusResponse> handleUpdateApplication(ApplicationIdsRequest request);
 
     void handleDeleteApplication(long id);
 
