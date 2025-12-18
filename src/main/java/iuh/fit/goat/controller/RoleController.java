b9d574e0ffe50b833fb5d13d59930b6611eee08e
trgcghai <conghai.tpma@gmail.com>
2025-12-18 13:57:10 +0700

    feat: implement role management functionality in Role service and controller

--- src/main/java/iuh/fit/goat/controller/RoleController.java
+++ src/main/java/iuh/fit/goat/controller/RoleController.java
@@ -20,70 +20,57 @@ import java.util.regex.Pattern;
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class RoleController {
-//    private final RoleService roleService;
-//
-//    @PostMapping("/roles")
-//    public ResponseEntity<Role> createRole(@Valid @RequestBody RoleCreateRequest role) {
-//        Role res = this.roleService.handleCreateRole(role);
-//        return ResponseEntity.status(HttpStatus.CREATED).body(res);
-//    }
-//
-//    @PutMapping("/roles")
-//    public ResponseEntity<Role> updateRole(@Valid @RequestBody Role role) throws InvalidException {
-//        if(this.roleService.handleGetRoleById(role.getRoleId()) == null){
-//            throw new InvalidException("Role doesn't exist");
-//        }
-//        Role res = this.roleService.handleUpdateRole(role);
-//        return ResponseEntity.status(HttpStatus.OK).body(res);
-//    }
-//
-//    @PutMapping("/roles/{id}/activate")
-//    public ResponseEntity<Role> activateRole(@PathVariable("id") String id) throws InvalidException {
-//        Pattern pattern = Pattern.compile("^[0-9]+$");
-//        if(!pattern.matcher(id).matches()){
-//            throw new InvalidException("Id is number");
-//        }
-//        Role res = this.roleService.handleActivateRole(Long.parseLong(id));
-//        return ResponseEntity.status(HttpStatus.OK).body(res);
-//    }
-//
-//    @PutMapping("/roles/{id}/deactivate")
-//    public ResponseEntity<Role> deactivateRole(@PathVariable("id") String id) throws InvalidException {
-//        Pattern pattern = Pattern.compile("^[0-9]+$");
-//        if(!pattern.matcher(id).matches()){
-//            throw new InvalidException("Id is number");
-//        }
-//        Role res = this.roleService.handleDeactivateRole(Long.parseLong(id));
-//        return ResponseEntity.status(HttpStatus.OK).body(res);
-//    }
-//
-//    @DeleteMapping("/roles/{id}")
-//    public ResponseEntity<Void> deleteRole(@PathVariable("id") String id) throws InvalidException {
-//        Pattern pattern = Pattern.compile("^[0-9]+$");
-//        if(!pattern.matcher(id).matches()){
-//            throw new InvalidException("Id is number");
-//        }
-//        if(this.roleService.handleGetRoleById(Long.parseLong(id)) == null){
-//            throw new InvalidException("Role doesn't exist");
-//        }
-//        this.roleService.handleDeleteRole(Long.parseLong(id));
-//        return ResponseEntity.status(HttpStatus.OK).body(null);
-//    }
-//
-//    @GetMapping("/roles/{id}")
-//    public ResponseEntity<Role> getRoleById(@PathVariable("id") long id) throws InvalidException {
-//        Role role = this.roleService.handleGetRoleById(id);
-//        if(role == null){
-//            throw new InvalidException("Role doesn't exist");
-//        }
-//        return ResponseEntity.status(HttpStatus.OK).body(role);
-//    }
-//
-//    @GetMapping("/roles")
-//    public ResponseEntity<ResultPaginationResponse> getAllRoles(
-//            @Filter Specification<Role> spec, Pageable pageable
-//    ) {
-//        ResultPaginationResponse result = this.roleService.handleGetAllRoles(spec, pageable);
-//        return ResponseEntity.status(HttpStatus.OK).body(result);
-//    }
+    private final RoleService roleService;
+
+    @PostMapping("/roles")
+    public ResponseEntity<Role> createRole(@Valid @RequestBody RoleCreateRequest role) {
+        Role res = this.roleService.handleCreateRole(role);
+        return ResponseEntity.status(HttpStatus.CREATED).body(res);
+    }
+
+    @PutMapping("/roles")
+    public ResponseEntity<Role> updateRole(@Valid @RequestBody Role role) throws InvalidException {
+        if(this.roleService.handleGetRoleById(role.getRoleId()) == null){
+            throw new InvalidException("Role doesn't exist");
+        }
+        Role res = this.roleService.handleUpdateRole(role);
+        return ResponseEntity.status(HttpStatus.OK).body(res);
+    }
+
+    @PutMapping("/roles/{id}/activate")
+    public ResponseEntity<Role> activateRole(@PathVariable("id") String id) throws InvalidException {
+        Pattern pattern = Pattern.compile("^[0-9]+$");
+        if(!pattern.matcher(id).matches()){
+            throw new InvalidException("Id is number");
+        }
+        Role res = this.roleService.handleActivateRole(Long.parseLong(id));
+        return ResponseEntity.status(HttpStatus.OK).body(res);
+    }
+
+    @PutMapping("/roles/{id}/deactivate")
+    public ResponseEntity<Role> deactivateRole(@PathVariable("id") String id) throws InvalidException {
+        Pattern pattern = Pattern.compile("^[0-9]+$");
+        if(!pattern.matcher(id).matches()){
+            throw new InvalidException("Id is number");
+        }
+        Role res = this.roleService.handleDeactivateRole(Long.parseLong(id));
+        return ResponseEntity.status(HttpStatus.OK).body(res);
+    }
+
+    @GetMapping("/roles/{id}")
+    public ResponseEntity<Role> getRoleById(@PathVariable("id") long id) throws InvalidException {
+        Role role = this.roleService.handleGetRoleById(id);
+        if(role == null){
+            throw new InvalidException("Role doesn't exist");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(role);
+    }
+
+    @GetMapping("/roles")
+    public ResponseEntity<ResultPaginationResponse> getAllRoles(
+            @Filter Specification<Role> spec, Pageable pageable
+    ) {
+        ResultPaginationResponse result = this.roleService.handleGetAllRoles(spec, pageable);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
 }
