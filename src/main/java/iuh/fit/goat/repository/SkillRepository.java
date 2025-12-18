f794c67bd590f9608b1710ca6e5244b290e32d20
trgcghai <conghai.tpma@gmail.com>
2025-12-18 15:00:38 +0700

    feat: implement skill management functionality in Skill service and controller

--- src/main/java/iuh/fit/goat/repository/SkillRepository.java
+++ src/main/java/iuh/fit/goat/repository/SkillRepository.java
@@ -10,6 +10,6 @@ import java.util.List;
 
 @Repository
 public interface SkillRepository extends JpaRepository<Skill, Long>, JpaSpecificationExecutor<Skill> {
-//    boolean existsByName(String name);
-//    List<Skill> findBySkillIdIn(List<Long> skillIds);
+    boolean existsByName(String name);
+    List<Skill> findBySkillIdIn(List<Long> skillIds);
 }
