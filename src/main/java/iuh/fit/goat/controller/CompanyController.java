aed04bb9da522d5113ed8be32e25231d70d472d7
MinhDat1312 <nguyenthangdat84@gmail.com>
2025-12-24 23:25:03 +0700

    feat: get company by name

--- src/main/java/iuh/fit/goat/controller/CompanyController.java
+++ src/main/java/iuh/fit/goat/controller/CompanyController.java
@@ -42,6 +42,18 @@ public class CompanyController {
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
+    @GetMapping("/slug/{name}")
+    public ResponseEntity<CompanyResponse> getCompanyByName(@PathVariable("name") String name) throws InvalidException {
+        String normalizedName = name.replace("-", " ");
+        Company company = this.companyService.handleGetCompanyByName(normalizedName);
+        if(company == null) {
+            throw new InvalidException("Company not found");
+        }
+
+        CompanyResponse response = this.companyService.convertToCompanyResponse(company);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
     @GetMapping
     public ResponseEntity<ResultPaginationResponse> getAllCompanies(
             @Filter Specification<Company> spec, Pageable pageable
