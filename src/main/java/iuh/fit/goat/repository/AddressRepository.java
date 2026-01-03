c49ff846f0933c51bfa18f99b60e6acf045e0f07
trgcghai <conghai.tpma@gmail.com>
2026-01-03 22:19:14 +0700

    feat: implement address management in applicant service, including create, update, and delete functionalities

---
+++ src/main/java/iuh/fit/goat/repository/AddressRepository.java
@@ -0,0 +1,8 @@
+package iuh.fit.goat.repository;
+
+import iuh.fit.goat.entity.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface AddressRepository extends JpaRepository<Address, Long>, JpaSpecificationExecutor<Address> {
+}
