684ef04385eb8c699433f8c207c906eb12b912ad
MinhDat1312 <nguyenthangdat84@gmail.com>
2026-01-13 13:32:54 +0700

    fix: allow ping controller

--- src/main/java/iuh/fit/goat/config/SecurityConfiguration.java
+++ src/main/java/iuh/fit/goat/config/SecurityConfiguration.java
@@ -108,9 +108,9 @@ public class SecurityConfiguration {
             RedisTokenBlacklistFilter redisTokenBlacklistFilter
     ) throws Exception {
         String[] whiteList = {
-                "/api/v1/ping",                       // Endpoint kiểm tra trạng thái server
-                "/api/v1/clear-cookies",              // Xóa toàn bộ cookies trên FE – không cần phân quyền
-                "/api/v1/uuid",                       // Tạo UUID cho user chưa đăng nhập – không cần phân quyền
+                "/ping",                       // Endpoint kiểm tra trạng thái server
+                "/clear-cookies",              // Xóa toàn bộ cookies trên FE – không cần phân quyền
+                "/uuid",                       // Tạo UUID cho user chưa đăng nhập – không cần phân quyền
                 "/api/v1/files/**",                   // Upload/Download file
 
                 "/",                                  // Trang gốc
