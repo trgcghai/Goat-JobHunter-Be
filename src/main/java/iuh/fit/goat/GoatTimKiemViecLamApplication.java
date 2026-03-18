8bfbf6af01fd19638bd29a3c21ad04d4076eefd7
MinhDat1312 <nguyenthangdat84@gmail.com>
2026-03-18 17:14:40 +0700

    chore: migrate database from AWS to Railway

--- src/main/java/iuh/fit/goat/GoatTimKiemViecLamApplication.java
+++ src/main/java/iuh/fit/goat/GoatTimKiemViecLamApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableScheduling
 public class GoatTimKiemViecLamApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
 		SpringApplication.run(GoatTimKiemViecLamApplication.class, args);
 
 	}
